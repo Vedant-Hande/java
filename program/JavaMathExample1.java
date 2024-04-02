485b56a987b5cf592f9fc6022f53c911c5444eb1
Vedant-Hande <148268306+Vedant-Hande@users.noreply.github.com>
2024-04-02 14:17:33 +0530

    commit

---
+++ program/JavaMathExample1.java
@@ -0,0 +1,19 @@
+public class JavaMathExample1 {
+    public static void main(String[] args) {
+        double x = 28;
+        double y = 4;
+
+        // return the maximum of two numbers
+        System.out.println("Maximum number of x and y is: " + Math.max(x, y));
+
+        // return the square root of y
+        System.out.println("Square root of y is: " + Math.sqrt(y));
+
+        // returns 28 power of 4 i.e. 28*28*28*28
+        System.out.println("Power of x and y is: " + Math.pow(x, y));
+
+        // return the logarithm of given value
+        System.out.println("Logarithm of x is: " + Math.log(x));
+        System.out.println("Logarithm of y is: " + Math.log(y));
+    }
+}
--- project jav/my project/MedicalStoreManagementSystem.java
+++ project jav/my project/MedicalStoreManagementSystem.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.util.ArrayList;
 
 class Medicine {
